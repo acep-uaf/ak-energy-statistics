ab7a1fc83bbaa734adb5d45e030574201aa37881
ianalexmac <iamacdougall@alaska.edu>
2026-07-15 20:03:13 -0800

    split data testing into quality and outlier

--- src/main.R
+++ src/main.R
@@ -1,5 +1,5 @@
 source('src/l0_extract.R')
-source('src/l1_test.R')
+source('src/l1_quality_check.R')
 source('src/l1_consolidate.R')
 source('src/l2_transform.R')
 
@@ -9,7 +9,7 @@ dir_delete('data/l2')
 
 l0_extract_pce_dir(dir_in = 'data/raw', pattern = 'raw_pce')
 
-l1_test_pce_dir(dir_in = 'data/l0_extracted/monthly', pattern = 'l0_pce')
+l1_check_quality_pce_dir(dir_in = 'data/l0_extracted/monthly', pattern = 'l0_pce', config = 'config/check_data/l1_pce_quality_check.yml')
 
 
 l1_consolidate_pce_data(path_with_pattern = "data/l1_tested/monthly/l1_pce_rate_line", join_by_columns = c("identifier", "line_no"))
